106ed0c59f5107a7c339706c54925fa6cb101f52
AchilleasB <90069683+AchilleasB@users.noreply.github.com>
2024-05-10 00:20:51 +0200

    added login endpoint

---
+++ backend/src/main/java/restapi/banking/app/dto/LoginResponseDTO.java
@@ -0,0 +1,17 @@
+package restapi.banking.app.dto;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponseDTO {
+    private UUID userId;
+    private String jwtToken;
+}
