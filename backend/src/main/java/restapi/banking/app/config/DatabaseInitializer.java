9ac818a8c5f045f74f9df179c3c6a1679b44e8ef
AchilleasB <90069683+AchilleasB@users.noreply.github.com>
2024-05-15 20:50:22 +0200

    initialized users and accounts

--- backend/src/main/java/restapi/banking/app/config/DatabaseInitializer.java
+++ backend/src/main/java/restapi/banking/app/config/DatabaseInitializer.java
@@ -1,5 +1,6 @@
 package restapi.banking.app.config;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
+import restapi.banking.app.model.Account;
+import restapi.banking.app.model.AccountType;
 import restapi.banking.app.model.User;
 import restapi.banking.app.model.UserRole;
 import restapi.banking.app.repository.UserRepository;
@@ -19,7 +22,7 @@ public class DatabaseInitializer {
     private final PasswordEncoder passwordEncoder;
 
     @PostConstruct // This method will be called after the bean has been initialized at the start of the application
-    public void initializeUsers() {
+    public void initializeData() {
         User superBank = new User();
         superBank.setFirstName("Super");
         superBank.setLastName("Bank");
@@ -27,7 +30,7 @@ public class DatabaseInitializer {
         superBank.setBsn("123456789");
         superBank.setEmail("superbank@email.com");
         superBank.setPassword(passwordEncoder.encode("password"));
-        superBank.setRole(UserRole.Employee);
+        superBank.setRole(UserRole.EMPLOYEE);
         superBank.setApproved(true);
         superBank.setDailyLimit(1000000);
 
@@ -40,11 +43,68 @@ public class DatabaseInitializer {
         customer1.setBsn("31855867");
         customer1.setEmail("achil@email.com");
         customer1.setPassword(passwordEncoder.encode("achil1234"));
-        customer1.setRole(UserRole.Customer);
+        customer1.setRole(UserRole.CUSTOMER);
         customer1.setApproved(true);
         customer1.setDailyLimit(5000);
 
         userRepository.saveAndFlush(customer1);
+
+        Account account1 = new Account();
+        account1.setIban("NL01INHO3456000001");
+        account1.setBalance(BigDecimal.valueOf(20560));
+        account1.setAccountType(AccountType.CHECKING);
+        account1.setOpeningDate(LocalDate.now());
+        account1.setUser(customer1);
+        account1.setAbsoluteLimit(BigDecimal.valueOf(100));
+        account1.setDailyLimit(BigDecimal.valueOf(5000));
+        account1.setTransactionLimit(BigDecimal.valueOf(1000));
+        account1.setActive(true);
+        
+        Account account2 = new Account();
+        account2.setIban("NL01INHO7634150001");
+        account2.setBalance(BigDecimal.valueOf(16450));
+        account2.setAccountType(AccountType.SAVINGS);
+        account2.setOpeningDate(LocalDate.now());
+        account2.setUser(customer1);
+        account2.setAbsoluteLimit(BigDecimal.valueOf(100));
+        account2.setDailyLimit(BigDecimal.valueOf(5000));
+        account2.setTransactionLimit(BigDecimal.valueOf(1000));
+        account2.setActive(true);
+
+        User customer2 = new User();
+        customer2.setFirstName("Tony");
+        customer2.setLastName("Stark");
+        customer2.setDateOfBirth(LocalDate.of(1970, 1, 1));
+        customer2.setBsn("132456378");
+        customer2.setEmail("stark@email.com");
+        customer2.setPassword(passwordEncoder.encode("stark1234"));
+        customer2.setRole(UserRole.CUSTOMER);
+        customer2.setApproved(true);
+        customer2.setDailyLimit(5000);
+
+        userRepository.saveAndFlush(customer2);
+
+        Account account3 = new Account();
+        account3.setIban("NL01INHO3456000001");
+        account3.setBalance(BigDecimal.valueOf(32480));
+        account3.setAccountType(AccountType.CHECKING);
+        account3.setOpeningDate(LocalDate.now());
+        account3.setUser(customer2);
+        account3.setAbsoluteLimit(BigDecimal.valueOf(100));
+        account3.setDailyLimit(BigDecimal.valueOf(5000));
+        account3.setTransactionLimit(BigDecimal.valueOf(1000));
+        account3.setActive(true);
+        
+        Account account4 = new Account();
+        account4.setIban("NL01INHO7634150001");
+        account4.setBalance(BigDecimal.valueOf(128450));
+        account4.setAccountType(AccountType.SAVINGS);
+        account4.setOpeningDate(LocalDate.now());
+        account4.setUser(customer2);
+        account4.setAbsoluteLimit(BigDecimal.valueOf(100));
+        account4.setDailyLimit(BigDecimal.valueOf(5000));
+        account4.setTransactionLimit(BigDecimal.valueOf(1000));
+        account4.setActive(true);
 
     }
 }
