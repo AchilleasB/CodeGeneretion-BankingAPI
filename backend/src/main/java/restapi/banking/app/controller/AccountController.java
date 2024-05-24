74e180295df65809f19a0968f5035b51721c8099
salmankanj <75369084+salmankanj@users.noreply.github.com>
2024-05-24 18:43:09 +0200

    Update AccountController.java

--- backend/src/main/java/restapi/banking/app/controller/AccountController.java
+++ backend/src/main/java/restapi/banking/app/controller/AccountController.java
@@ -2,6 +2,8 @@ package restapi.banking.app.controller;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
