2b3aa6e0445ab9c839350142be3a1336911211a7
AchilleasB <90069683+AchilleasB@users.noreply.github.com>
2024-06-09 22:43:06 +0200

    path for deployed version

--- frontend/src/axios-auth.js
+++ frontend/src/axios-auth.js
@@ -3,6 +3,7 @@ import { useAuthStore } from './stores/auth';
 
 const instance = axios.create({
     baseURL: 'http://localhost:8080/'
+    // baseURL: import.meta.env.VITE_API_BASE_URL // This for the deployed version
 });
 
 // Add a request interceptor to include the JWT token in the headers
