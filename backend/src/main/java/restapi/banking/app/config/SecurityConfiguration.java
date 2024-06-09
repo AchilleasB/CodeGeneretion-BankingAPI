6047b2d3b2a884ed7d5cdf99fd8871599ab5a7bd
AchilleasB <90069683+AchilleasB@users.noreply.github.com>
2024-06-09 22:07:47 +0200

    Update SecurityConfiguration.java

--- backend/src/main/java/restapi/banking/app/config/SecurityConfiguration.java
+++ backend/src/main/java/restapi/banking/app/config/SecurityConfiguration.java
@@ -60,7 +60,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("https://achilleasb.github.io"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
